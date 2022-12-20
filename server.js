4afe2b569647b6b510399b2c67393a38461b520a
PaytonRay <paytonray0810@gmail.com>
2022-12-19 22:38:48 -0500

    having to change my own code and break it down

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-const PORT = process.env.PORT || 3001;
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/apiRoutes/index');
 
-app.use(express.urlencoded({
-    extended: true
-}));
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+    const deleteNote = notes.filter((removeNote) => removeNote.id !== req.params.id);
+    fs.writeFileSync("/db/db.json", JSON.stringify(deleteNote));
+    res.json(deleteNote)
+});
+
+app.listen(PORT, function () {
+    console.log("Listing on port " + PORT);
 });
